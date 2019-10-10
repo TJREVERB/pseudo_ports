e319186b5d606633f41d63b2e92dee939722a50d
=Shreepa Parthaje <shreepa.parthaje@gmail.com>
2019-10-10 16:25:24 -0400

    Iridium Device

--- devices/iridium_device.py
+++ devices/iridium_device.py
@@ -5,14 +5,14 @@ from threading import Thread
 from . import Device
 
 
-class APRS(Device):
+class Iridium(Device):
 
     def __init__(self):
-        Device.__init__(self, 'APRS')
+        Device.__init__(self, 'Iridium')
         data_writer = Thread(target=self.writer)
         data_writer.start()
 
     def writer(self):
         while True:
-            write(self.master, b'w')
+            write(self.master, b'q')
             sleep(2)
--- test_device.py
+++ test_device.py
@@ -1,10 +1,15 @@
 from serial import Serial
 from devices.aprs_device import APRS
+from devices.iridium_device import Iridium
 
-device = APRS()
+aprs = APRS()
+iridium = Iridium()
 
-s = Serial(port=device.port_name)
+aprs_s = Serial(port=aprs.port_name)
+i_s = Serial(port=iridium.port_name)
 
 while True:
-    s.write(b'r')
-    print(f"APRS Wrote {s.read()}")
+    # aprs_s.write(b'r')
+    # i_s.write(b'i')
+    print(f"APRS Wrote {aprs_s.read()}")
+    print(f"Iridium Wrote {aprs_s.read()}")
